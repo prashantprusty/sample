330b6abbcb8bc7743621cf4d863e462aee3dcde0
DELL-PC <DELL-PC@192.168.0.4>
2018-07-24 21:20:49 +0530

    ok

--- src/test/java/PP/Training/PP/MProj1/Testing.java
+++ src/test/java/PP/Training/PP/MProj1/Testing.java
@@ -4,8 +4,8 @@ import org.testng.annotations.Test;
 
 public class Testing {
   @Test
-  public void f() {
-	  
+  public void f() { 
 	 System.out.println("Hello");
+	 System.out.println("Hello12121212");
   }
 }
